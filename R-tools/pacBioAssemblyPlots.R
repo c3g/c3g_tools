f30195f39fc2593d22702671c475d2607234b089
Julien Tremblay <julien.tremblay@mail.mcgill.ca>
2013-11-24 19:39:15 -0500

    Minor fix to plot script. BFXDEV-30

--- R-tools/pacBioAssemblyPlots.R
+++ R-tools/pacBioAssemblyPlots.R
@@ -1,26 +1,3 @@
-usage=function(errM) {
-        cat("\nUsage : Rscript pacBioAssemblyPlots.R [option] <Value>\n")
-        cat("       -i        : infile\n")
-        cat("       -o        : outdir\n")
-}
-
-ARG = commandArgs(trailingOnly = T)
-
-if(length(ARG) < 2) {
-	usage("missing arguments")
-}
-
-## get arg variables
-for (i in 1:length(ARG)) {
-	if (ARG[i] == "-i") {
-		infile=ARG[i+1]
-	} else if (ARG[i] == "-o") {
-		outfile=ARG[i+1]
-	}
-}
-
-pacBioPlots(infile, outdir)
-
 # Function that takes a infile: (filtered_summary.csv) and an outdir where to
 # To write plot files.
 pacBioPlots <- function(infile, outdir) {
@@ -72,3 +49,27 @@ pacBioPlots <- function(infile, outdir) {
   print(p)
   dev.off()
 } 
+
+usage=function(errM) {
+        cat("\nUsage : Rscript pacBioAssemblyPlots.R [option] <Value>\n")
+        cat("       -i        : infile\n")
+        cat("       -o        : outdir\n")
+}
+
+ARG = commandArgs(trailingOnly = T)
+
+if(length(ARG) < 2) {
+	usage("missing arguments")
+}
+
+## get arg variables
+for (i in 1:length(ARG)) {
+	if (ARG[i] == "-i") {
+		infile=ARG[i+1]
+	} else if (ARG[i] == "-o") {
+		outdir=ARG[i+1]
+	}
+}
+
+pacBioPlots(infile, outdir)
+
