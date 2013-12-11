1c1ca049485f8393a2aa9c5547b0e284a443c46c
Joël Fillon <joel.fillon@mcgill.ca>
2013-12-11 09:44:14 -0500

    Added in log report lowest/highest memory jobs, queue, limits and many other columns

--- perl-tools/getLogReport.pl
+++ perl-tools/getLogReport.pl
@@ -84,9 +84,18 @@ sub readJobLogListFile {
           # Job MUGQIC exit status
           } elsif ($jobLine =~ /MUGQICexitStatus:(\d+)/) {
             $jobLog{'MUGQICexitStatus'} = $1;
-          # Job exit status (should be the same as MUGQIC exit status unless MUGQIC exit status is skipped)
-          } elsif ($jobLine =~ /^Exit_status:\s+(\d+)/) {
-            $jobLog{'exitStatus'} = $1;
+          # Username
+          } elsif ($jobLine =~ /^Username:\s+(\S+)/) {
+            $jobLog{'username'} = $1;
+          # Group
+          } elsif ($jobLine =~ /^Group:\s+(\S+)/) {
+            $jobLog{'group'} = $1;
+          # Session
+          } elsif ($jobLine =~ /^Session:\s+(\S+)/) {
+            $jobLog{'session'} = $1;
+          # Limits
+          } elsif ($jobLine =~ /^Limits:\s+(\S+)/) {
+            $jobLog{'limits'} = $1;
           # Job used resources
           } elsif ($jobLine =~ /^Resources:\s+cput=(\S+),mem=(\S+),vmem=(\S+),walltime=(\d+:\d+:\d+)/) {
             $jobLog{'cput'} = $1;
@@ -95,6 +104,18 @@ sub readJobLogListFile {
             $jobLog{'walltime'} = $4;
             # Compute duration in seconds from walltime hours, minutes, seconds
             $jobLog{'duration'} = timeToSeconds($4);
+          # Queue
+          } elsif ($jobLine =~ /^Queue:\s+(\S+)/) {
+            $jobLog{'queue'} = $1;
+          # Account
+          } elsif ($jobLine =~ /^Account:\s+(\S+)/) {
+            $jobLog{'account'} = $1;
+          # Job exit status (should be the same as MUGQIC exit status unless MUGQIC exit status is skipped)
+          } elsif ($jobLine =~ /^Exit_status:\s+(\d+)/) {
+            $jobLog{'exitStatus'} = $1;
+          # Nodes
+          } elsif ($jobLine =~ /^Nodes:\s+(\S+)/) {
+            $jobLog{'nodes'} = $1;
           # Job end date
           } elsif ($jobLine =~ /^End PBS Epilogue (.*) (\d+)$/) {
             $jobLog{'endDate'} = $1;
@@ -122,11 +143,13 @@ sub getLogTextReport {
 
   $logTextReport .= "# Number of jobs: " . ($#AoH_jobLogList + 1) . "\n#\n";
 
-  # Retrieve first job start date, last job end date, shortest job, longest job
+  # Retrieve first job start date, last job end date, shortest/longest jobs, lowest/highest memory jobs
   my $firstStartSecondsSinceEpoch;
   my $lastEndSecondsSinceEpoch;
   my $shortestJob;
   my $longestJob;
+  my $lowestMemoryJob;
+  my $highestMemoryJob;
 
   for my $jobLog (@AoH_jobLogList) {
     if (exists $jobLog->{'startSecondsSinceEpoch'} and (not defined $firstStartSecondsSinceEpoch or $firstStartSecondsSinceEpoch > $jobLog->{'startSecondsSinceEpoch'})) {
@@ -143,6 +166,14 @@ sub getLogTextReport {
         $longestJob = $jobLog;
       }
     }
+    if (exists $jobLog->{'mem'}) {
+      if (not defined $lowestMemoryJob or kiBToNum($lowestMemoryJob->{'mem'}) > kiBToNum($jobLog->{'mem'})) {
+        $lowestMemoryJob = $jobLog;
+      }
+      if (not defined $highestMemoryJob or kiBToNum($highestMemoryJob->{'mem'}) < kiBToNum($jobLog->{'mem'})) {
+        $highestMemoryJob = $jobLog;
+      }
+    }
   }
 
   # Print out execution time
@@ -153,7 +184,13 @@ sub getLogTextReport {
 
   # Print out shortest and longest jobs
   $logTextReport .= "# Shortest job: " . (defined $shortestJob ? $shortestJob->{'jobName'} . " (" . formatDuration($shortestJob->{'duration'}) . ")" : "N/A") . "\n";
-  $logTextReport .= "# Longest job: " . (defined $longestJob ? $longestJob->{'jobName'} . " (" . formatDuration($longestJob->{'duration'}) . ")" : "N/A") . "\n#\n";
+  $logTextReport .= "# Longest job: " . (defined $longestJob ? $longestJob->{'jobName'} . " (" . formatDuration($longestJob->{'duration'}) . ")" : "N/A") . "\n";
+  $logTextReport .= "#\n";
+
+  # Print out lowest and highest memory jobs
+  $logTextReport .= "# Lowest memory job: " . (defined $lowestMemoryJob ? $lowestMemoryJob->{'jobName'} . " (" . sprintf("%.2f", kiBToGiB($lowestMemoryJob->{'mem'})) . " GiB" . ")" : "N/A") . "\n";
+  $logTextReport .= "# Highest memory job: " . (defined $highestMemoryJob ? $highestMemoryJob->{'jobName'} . " (" . sprintf("%.2f", kiBToGiB($highestMemoryJob->{'mem'})) . " GiB" . ")" : "N/A") . "\n";
+  $logTextReport .= "#\n";
 
   $logTextReport .= join("\t", (
     "#JOB_ID",
@@ -170,6 +207,13 @@ sub getLogTextReport {
     "PHYSICAL_MEM",
     "VIRTUAL_MEM",
     "EXTRA_VIRTUAL_MEM_PCT",
+    "LIMITS",
+    "QUEUE",
+    "USERNAME",
+    "GROUP",
+    "SESSION",
+    "ACCOUNT",
+    "NODES",
     "PATH"
   )) . "\n";
 
@@ -186,9 +230,16 @@ sub getLogTextReport {
       exists $jobLog->{'endSecondsSinceEpoch'} ? strftime('%FT%T', localtime($jobLog->{'endSecondsSinceEpoch'})) : "N/A",
       exists $jobLog->{'cput'} ? $jobLog->{'cput'} . " (" . formatDuration(timeToSeconds($jobLog->{'cput'})) . ")" : "N/A",
       (exists $jobLog->{'walltime'} and exists $jobLog->{'cput'} and timeToSeconds($jobLog->{'walltime'}) != 0) ? sprintf("%.2f", timeToSeconds($jobLog->{'cput'}) / timeToSeconds($jobLog->{'walltime'})) : "N/A",
-      exists $jobLog->{'mem'} ? sprintf("%.1f", kiBToGiB($jobLog->{'mem'})) . " GiB" : "N/A",
-      exists $jobLog->{'vmem'} ? sprintf("%.1f", kiBToGiB($jobLog->{'vmem'})) . " GiB" : "N/A",
+      exists $jobLog->{'mem'} ? sprintf("%.2f", kiBToGiB($jobLog->{'mem'})) . " GiB" : "N/A",
+      exists $jobLog->{'vmem'} ? sprintf("%.2f", kiBToGiB($jobLog->{'vmem'})) . " GiB" : "N/A",
       (exists $jobLog->{'vmem'} and exists $jobLog->{'mem'} and kiBToGiB($jobLog->{'mem'}) != 0) ? sprintf("%.1f", (kiBToGiB($jobLog->{'vmem'}) / kiBToGiB($jobLog->{'mem'}) - 1) * 100) . " %" : "N/A",
+      exists $jobLog->{'limits'} ? $jobLog->{'limits'} : "N/A",
+      exists $jobLog->{'queue'} ? $jobLog->{'queue'} : "N/A",
+      exists $jobLog->{'username'} ? $jobLog->{'username'} : "N/A",
+      exists $jobLog->{'group'} ? $jobLog->{'group'} : "N/A",
+      exists $jobLog->{'session'} ? $jobLog->{'session'} : "N/A",
+      exists $jobLog->{'account'} ? $jobLog->{'account'} : "N/A",
+      exists $jobLog->{'nodes'} ? $jobLog->{'nodes'} : "N/A",
       exists $jobLog->{'path'} ? $jobLog->{'path'} : "N/A"
     )) . "\n";
   }
@@ -226,6 +277,16 @@ sub formatDuration {
   # 1 day or more
   else {
     return int($seconds / (60 * 60 * 24)) . " d " . formatDuration($seconds % (60 * 60 * 24));
+  }
+}
+
+sub kiBToNum {
+  my $size = shift;
+
+  if ($size =~ /^(\d+)kb/) {
+    return $1;
+  } else {
+    return "N/A";
   }
 }
 
