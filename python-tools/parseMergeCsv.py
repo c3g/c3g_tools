65337cf2db540bcfbb448fd1bbb5f900232afb37
Johanna Sandoval <johanna.sandoval@mail.mcgill.ca>
2015-05-28 17:16:18 -0400

    BFXDEV-396 parse trinotate output, read isoforms length and print only the longest isoform per gene in annotations, added a standard parse plus merge csv files python program

---
+++ python-tools/parseMergeCsv.py
@@ -0,0 +1,125 @@
+#!/usr/bin/env python
+
+################################################################################
+# Copyright (C) 2014, 2015 GenAP, McGill University and Genome Quebec Innovation Centre
+#
+# This file is part of MUGQIC Pipelines tools.
+#
+# MUGQIC Pipelines is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# MUGQIC Pipelines is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with MUGQIC Pipelines.  If not, see <http://www.gnu.org/licenses/>.
+################################################################################
+
+import csv
+import argparse
+import string
+import sys
+import os
+import collections
+import warnings
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(formatter_class=argparse.RawTextHelpFormatter)
+    parser.add_argument("-i", "--input_files", help="List of files to merge" , type=str, nargs="+", required=True)
+    parser.add_argument("-d", "--delimiter", help="Delmiter for input files" , type=str, nargs="+", required=False, default='\t')    
+    parser.add_argument("-o", "--output", help="Output File prefix", type=str , required=True)
+    parser.add_argument("-c", "--common", help="Common columns, if more than one, use comma separated column names, i.e. gene,transcript ", nargs="+",  type=str , required=True)
+    parser.add_argument("-s", "--subset", help="A subset of column names to print in the output file", nargs="+",  type=str , required=False, default=None)
+    parser.add_argument("-x", "--exclude", help="A subset of column names to exclude from  the output file", nargs="+",  type=str , required=False, default=None)
+    parser.add_argument("-l", "--left", help="left outer join", action="store_true")
+    parser.add_argument("-t", "--sort", help="a column name to sort by", type=str , required=False)
+    args = parser.parse_args()
+    
+    #error found when testing, don't ask me if system size is exceeded
+    csv.field_size_limit(sys.maxsize)
+    
+    # Parameters
+    infiles=args.input_files
+    outfile=args.output
+    subset=args.subset
+    exclude=args.exclude
+    
+    # Separator for output file is \t
+    out_sep='\t'
+    in_sep=[]
+    
+    # Separator , common must have the same length as infiles
+    if len(args.delimiter) == len(infiles) and len(args.delimiter) > 1:
+        in_sep=args.delimiter
+    elif len(args.delimiter) == 1:
+        in_sep=[args.delimiter[0]] * len(infiles)
+    else:
+        raise Exception("Error: Two or more input files are required " + args.delimiter )
+ 
+    if len(args.common) == len(infiles) and len(args.common) > 1:
+        key=args.common
+    elif len(args.common) == 1:
+        key=[args.common[0]] * len(infiles)
+    else:
+        key=[None] * len(infiles)
+        
+    #print str(in_sep)
+    #print str(key)
+    #print str(args.left)
+    # Open output files    
+    data = collections.OrderedDict()
+    fieldnames = []
+    i=0
+    for filename in infiles:
+        with open(filename, "rb") as fp: # python 2
+            if in_sep[i] in ['\t',"\\t"]:                
+                reader = csv.DictReader(fp, quoting=csv.QUOTE_NONE, dialect=csv.excel_tab)
+            else:
+                reader = csv.DictReader(fp, delimiter=in_sep[i],quoting=csv.QUOTE_NONE)
+            fieldnames.extend(reader.fieldnames)
+            for row in reader:
+                key_field = out_sep.join(row[k] for k in key[i].split(",") ) if key[i] else reader.fieldnames[0]
+                # Default behavior is cross join (common elements of all tables are added)
+                # if left outer join, preserve only keys of the first file
+                if (args.left and i == 0 ) or not args.left:
+                    data.setdefault(key_field , {}).update(row)
+                elif data.has_key(key_field) :
+                    data[key_field].update(row)
+            del reader
+        i+=1
+        
+    if subset and subset <= fieldnames:
+      fieldnames = subset
+    else:
+      fieldnames = list(data.fromkeys(fieldnames))
+    diff = [x for x in exclude if x not in fieldnames]
+    #print str(fieldnames)
+    #print str(exclude)
+    #print str(diff)
+    if exclude and len(diff) > 0 :
+        error_exclude = " ".join(diff)        
+        warnings.warn("WARNING: Columns to exclude are not in input files: " + error_exclude )
+    else:        
+        fieldnames = [x for x in fieldnames if x not in exclude ]
+    
+    # Sort by a selected field
+    if args.sort and args.sort in fieldnames:
+        tmpdata=collections.OrderedDict()
+        try:
+            # Try numeric conversion
+            tmpdata=sorted(data.items(), key=lambda d: float(d[1][args.sort]))            
+            data=collections.OrderedDict(tmpdata)
+        except ValueError:            
+            tmpdata=sorted(data.items(), key=lambda d: d[1][args.sort])
+            data=collections.OrderedDict(tmpdata)
+            
+    # Print output file  
+    with open(outfile, "wb") as fp:
+        writer = csv.writer(fp, delimiter=out_sep)
+        writer.writerow(fieldnames)
+        for row in data.itervalues():
+            writer.writerow([row.get(field, '') for field in fieldnames])
--- python-tools/parseTrinotateOutput.py
+++ python-tools/parseTrinotateOutput.py
@@ -24,6 +24,9 @@ import argparse
 import string
 import sys
 import os
+import collections
+import itertools
+import operator
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(formatter_class=argparse.RawTextHelpFormatter)
@@ -32,6 +35,7 @@ if __name__ == '__main__':
     parser.add_argument("-b", "--Top_BLASTX_hit", help="Column name for top blast hit, default is sprot_Top_BLASTX_hit", type=str , required=False, default= "sprot_Top_BLASTX_hit")
     parser.add_argument("-g", "--gene_ontology", help="Name of column for gene_ontology, default is gene_ontology_blast", type=str, required=False, default= "gene_ontology_blast")
     parser.add_argument("-o", "--output", help="Output File prefix", type=str , required=True)
+    parser.add_argument("-l", "--length_file", help="Path to the gene/transcript length file. If declared, only the longest gene/transcript is reported. (\t separated file with columns gene_id\tlength)", type=argparse.FileType('rb') , required=False)
     args = parser.parse_args()
     
     csv.field_size_limit(sys.maxsize)
@@ -42,31 +46,73 @@ if __name__ == '__main__':
     go=args.gene_ontology
     outfile=args.output
     key=args.item_column
+    length_fname=args.length_file
     
     #Open output files
     outfile_blast=open(os.path.abspath(outfile) + "_blast.tsv" , "wb")
     outfile_go=open(os.path.abspath(outfile) + "_go.tsv" , "wb")
     
     # Read file    
-    reader = csv.DictReader(infile,  delimiter='\t', quoting=csv.QUOTE_NONE)    
-    field_names_blast= [key] + [blast + "_ext"] + [vals for vals in reader.fieldnames if vals not in [key] ]
+    trinotatereader = csv.DictReader(infile,  delimiter='\t', quoting=csv.QUOTE_NONE)    
+    field_names_blast= [key] + ["Symbol"] + [vals for vals in trinotatereader.fieldnames if vals not in [key] ] + ["longest_transcript_length", "longest_transcript_id"]
     field_names_go=[key] + [go]
     csvwriter = csv.DictWriter(outfile_blast, field_names_blast, extrasaction='ignore',  delimiter='\t', quoting=csv.QUOTE_NONE)
     csvwriter_go = csv.DictWriter(outfile_go, field_names_go, extrasaction='ignore',  delimiter='\t', quoting=csv.QUOTE_NONE)
     
-    # Write blast annotated results
+    # Force to print only one line per transcript id
+    transcript_id = trinotatereader.fieldnames[1]
+    
+    # Read transcript length file. If this file is defined, the output will be
+    # a gene/isoform subset of Trinotate report with only the longest isoform per gene.
+    longest_item = {}
+    length_id = "length"
+    
+    if length_fname :
+        length_map = collections.defaultdict(dict)
+        lengthreader = csv.DictReader(length_fname,  delimiter='\t', quoting=csv.QUOTE_NONE)
+        item_id = lengthreader.fieldnames[0]
+        length_id = lengthreader.fieldnames[1]        
+        for row in lengthreader:
+            length_map[row[item_id]] = row
+            length_map[row[item_id]].update(dict( gene_group = "_".join(row[item_id].split("_")[:2]) ))
+        group_by='gene_group'
+        # Painfully sorting by gene ID
+        rows = sorted(length_map.values(), key=lambda d: d[group_by])
+        # Grouping and obtaining the max length
+        groups = itertools.groupby(rows, lambda d: d[group_by])
+        for k,g in groups:
+            longest_item[k]=max(g, key=lambda d: float(d[length_id]))
+        #longest_item = dict( k, max(g, key=lambda d: float(d[length_id])) for k, g in groups)
+        # Free 
+        del lengthreader
+        del groups
+        del rows
+        length_fname.close()
+    #print "NOTICE: length of longest_item: " + str(len(longest_item))
+    count =1
+             
+    # Write blast / go annotated results
+    item_done={}
     csvwriter.writeheader()
     csvwriter_go.writeheader()
-    for line in reader:
-        best_blast=line[blast].split("^")[0]           
-        line[blast + "_ext" ] = best_blast        
-        csvwriter.writerow(line)
-        go_lines=string.split(line[go], "`")            
-        for go_line in go_lines:
-            go_table=go_line.split("^")
-            line[go]=go_table[0]
-            if go_table[0] != ".":
-                csvwriter_go.writerow(line)
-    del reader
+    for line in iter(trinotatereader):
+        if item_done.has_key(line[transcript_id]):
+            continue
+        else:
+            item_done[line[transcript_id]] = None
+        line["gene_group"] = "_".join(line[key].split("_")[:2])
+        if len(longest_item) == 0 or ( len(longest_item) > 0  and longest_item.has_key(line["gene_group"]) and longest_item[line["gene_group"]][transcript_id] == line[transcript_id] ) :
+            best_blast=line[blast].split("^")[0]           
+            line["Symbol"] = best_blast        
+            line["longest_transcript_length"] = longest_item[line["gene_group"]][length_id] if longest_item.has_key(line["gene_group"]) else None
+            line["longest_transcript_id"] = longest_item[line["gene_group"]][transcript_id] if longest_item.has_key(line["gene_group"]) else None
+            csvwriter.writerow(line)
+            go_lines=string.split(line[go], "`")
+            for go_line in go_lines:
+                go_table=go_line.split("^")
+                line[go]=go_table[0]
+                if go_table[0] != ".":
+                    csvwriter_go.writerow(line)
+    del trinotatereader
     del csvwriter
     del csvwriter_go
