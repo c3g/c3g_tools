bf43c15efb3d4169cfce4ed3454b70b6dbba56ab
Edouard Henrion <edouard.henrion@mcgill.ca>
2017-08-17 15:45:52 -0400

    while working on BFXDEV-172, did a minor review or goseq.R and edger.R : updated indentation for more clarity when reading the code

--- R-tools/goseq.R
+++ R-tools/goseq.R
@@ -7,20 +7,20 @@
 
 
 usage=function(errM) {
-        cat("\nUsage : Rscript goseq.R [option] <Value>\n")
-        cat("       -d        : file containing the dge results\n")
-        cat("       -c        : columns to use for input (first column = geneSymbol list, second column= adjusted p-values\n")
-        cat("       -f        : FDR threshold detection of pathway enrichement\n")
-        cat("       -p        : p-value threshold for e feature to be included in the GO analysis\n")
-	cat("       -k        : known genome gene name mapping file\n")
-	cat("       -m        : maximum number of pathway return (default all)\n")
-	cat("       -v        : DGE method 0: edger/deseq ; 1: cuffdiff (default 0)\n")
-	cat("       -i        : gene Name ID (default geneSymbol)\n")
-	cat("       -G        : Nonnative Go file path\n")
-	cat("       -a        : Gene length file path\n")
-	cat("       -o        : output directory\n")
-        cat("       -h        : this help\n\n")
-        stop(errM)
+    cat("\nUsage : Rscript goseq.R [option] <Value>\n")
+    cat("       -d        : file containing the dge results\n")
+    cat("       -c        : columns to use for input (first column = geneSymbol list, second column= adjusted p-values\n")
+    cat("       -f        : FDR threshold detection of pathway enrichement\n")
+    cat("       -p        : p-value threshold for e feature to be included in the GO analysis\n")
+    cat("       -k        : known genome gene name mapping file\n")
+    cat("       -m        : maximum number of pathway return (default all)\n")
+    cat("       -v        : DGE method 0: edger/deseq ; 1: cuffdiff (default 0)\n")
+    cat("       -i        : gene Name ID (default geneSymbol)\n")
+    cat("       -G        : Nonnative Go file path\n")
+    cat("       -a        : Gene length file path\n")
+    cat("       -o        : output directory\n")
+    cat("       -h        : this help\n\n")
+    stop(errM)
 }
 
 
@@ -42,38 +42,38 @@ gene_type="geneSymbol"
 
 ## get arg variables
 for (i in 1:length(ARG)) {
-        if (ARG[i] == "-d") {
-                file=ARG[i+1]
-        } else if (ARG[i] == "-c") {
-                columns=ARG[i+1]
-        } else if (ARG[i] == "-f") {
-                fdrThr=as.numeric(ARG[i+1])
-	} else if (ARG[i] == "-p") {
-                pvalThr=as.numeric(ARG[i+1])
-	} else if (ARG[i] == "-o") {
-                out_path=ARG[i+1]
-	} else if (ARG[i] == "-k") {
-                known_ref=ARG[i+1]
-	} else if (ARG[i] == "-m") {
-                maxP=as.numeric(ARG[i+1])
-	} else if (ARG[i] == "-v") {
-                method=as.numeric(ARG[i+1])
-	} else if (ARG[i] == "-G") {
-                go_path=ARG[i+1]
-	} else if (ARG[i] == "-a") {
-                gene_path=ARG[i+1]
-	} else if (ARG[i] == "-i") {
-                gene_type=ARG[i+1]
-	} else if (ARG[i] == "-h") {
-                usage("")
-        }
+    if (ARG[i] == "-d") {
+        file=ARG[i+1]
+    } else if (ARG[i] == "-c") {
+        columns=ARG[i+1]
+    } else if (ARG[i] == "-f") {
+        fdrThr=as.numeric(ARG[i+1])
+    } else if (ARG[i] == "-p") {
+        pvalThr=as.numeric(ARG[i+1])
+    } else if (ARG[i] == "-o") {
+        out_path=ARG[i+1]
+    } else if (ARG[i] == "-k") {
+        known_ref=ARG[i+1]
+    } else if (ARG[i] == "-m") {
+        maxP=as.numeric(ARG[i+1])
+    } else if (ARG[i] == "-v") {
+        method=as.numeric(ARG[i+1])
+    } else if (ARG[i] == "-G") {
+        go_path=ARG[i+1]
+    } else if (ARG[i] == "-a") {
+        gene_path=ARG[i+1]
+    } else if (ARG[i] == "-i") {
+        gene_type=ARG[i+1]
+    } else if (ARG[i] == "-h") {
+        usage("")
+    }
 }
 ## check arg consitency
 if (!(file.exists(file))) {
-	stop("Input file not found") 
+    stop("Input file not found") 
 }
 if (out_path == "") {
-	stop("Output directory not found")
+    stop("Output directory not found")
 }
 
 
@@ -101,8 +101,8 @@ d2<-d2[order(d2[,2]),]
 head(d2)
 is.significant<-function(x,pv=pvalThr) ifelse(x <= pv,1,0)
 if(sum(is.significant(d2[,2])==1) == 0) {
-	print("No significant adjusted p-values found")
-	write.table(paste("Enriched category",paste("FDR <",as.character(fdrThr),"filtered p-value"),"GOID","Term","Ontology","Definition","Synonym", sep="\t"), out_path, append=F, row.names=F, col.names=F, quote=F)
+    print("No significant adjusted p-values found")
+    write.table(paste("Enriched category",paste("FDR <",as.character(fdrThr),"filtered p-value"),"GOID","Term","Ontology","Definition","Synonym", sep="\t"), out_path, append=F, row.names=F, col.names=F, quote=F)
     q("no",0)
 }
 d3<-cbind(d2[,1], is.significant(d2[,2]))
@@ -122,9 +122,9 @@ enriched.GO = cbind(GO.wall$category[p.adjust(GO.wall$over_represented_pvalue, m
 head(enriched.GO)
 library(GO.db)
 if(dim(enriched.GO)[1] == 0) {
-	print(paste("No significant (FDR <",as.character(fdrThr),") enriched categories"))
-	write.table(paste("Enriched category",paste("FDR <",as.character(fdrThr),"filtered p-value"),"GOID","Term","Ontology","Definition","Synonym", sep="\t"), out_path, append=F, row.names=F, col.names=F, quote=F)
-	q("no",0)
+    print(paste("No significant (FDR <",as.character(fdrThr),") enriched categories"))
+    write.table(paste("Enriched category",paste("FDR <",as.character(fdrThr),"filtered p-value"),"GOID","Term","Ontology","Definition","Synonym", sep="\t"), out_path, append=F, row.names=F, col.names=F, quote=F)
+    q("no",0)
 } else {
 if (maxP == -1) maxP=dim(enriched.GO)[1]
 }
@@ -133,7 +133,7 @@ write.table(paste("Enriched category",paste("FDR <",as.character(fdrThr),"filter
 for (i in 1:maxP) {
 f<-GOTERM[[enriched.GO[i,1]]]
 if (!is.null(f)){
-	write.table(paste(i, enriched.GO[i,2], GOID(f), Term(f), Ontology(f), Definition(f), Synonym(f)[i], sep="\t"), out_path, append=T, row.names=F, col.names=F, quote=F)
+    write.table(paste(i, enriched.GO[i,2], GOID(f), Term(f), Ontology(f), Definition(f), Synonym(f)[i], sep="\t"), out_path, append=T, row.names=F, col.names=F, quote=F)
 }
 }
 
