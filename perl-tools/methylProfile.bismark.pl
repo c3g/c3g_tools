957de3643e3481ca8a1abc040c5c67efdee82f3f
Edouard Henrion <edouard.henrion@mcgill.ca>
2017-08-17 15:48:04 -0400

    BFXDEV-674 - perl tool methylProfile.bismark.pl minor update

--- perl-tools/methylProfile.bismark.pl
+++ perl-tools/methylProfile.bismark.pl
@@ -13,53 +13,59 @@ use Env;
 use Getopt::Long;
 use PerlIO::gzip;
 
-my $help="";
-my $input="";
-my $output="";
-my $total_reads=0;
+my $help = "";
+my $input = "";
+my $output = "";
+my $total_reads = 0;
 
 GetOptions (
-    'help'          => \$help,
-    'i|input=s'     => \$input,
-    'o|output=s'    => \$output,
-    );
+    'help'       => \$help,
+    'i|input=s'  => \$input,
+    'o|output=s' => \$output,
+);
+
 if ($help) {
- print_helpfile();
- exit();
+    print_helpfile();
+   exit();
 }
 
-my $temp="";
-my @temparray=();
-my %CpGprofileHash=();
-my %CpGfwHash=();
-my %CpGrvHash=();
-my %CpGfwReadsHash=();
-my %CpGrvReadsHash=();
-my %CpGfwMethReadsHash=();
-my %CpGrvMethReadsHash=();
+die "--input missing\n" unless($input);
+die "--output missing\n" unless($output);
 
-my $strand="";
-my $fwcoordinate=0;
-my $CpGid="";
-my %CpGtotalReadsHash=();
-my %CpGtotalMethReadsHash=();
-my $total_meth=0;
-my $fw_meth=0;
-my $rv_meth=0;
+die "Input file $input does not exist...\n" unless(-e $input);
+
+my $temp = "";
+my @temparray = ();
+my %CpGprofileHash = ();
+my %CpGfwHash = ();
+my %CpGrvHash = ();
+my %CpGfwReadsHash = ();
+my %CpGrvReadsHash = ();
+my %CpGfwMethReadsHash = ();
+my %CpGrvMethReadsHash = ();
+
+my $strand = "";
+my $fwcoordinate = 0;
+my $CpGid = "";
+my %CpGtotalReadsHash = ();
+my %CpGtotalMethReadsHash = ();
+my $total_meth = 0;
+my $fw_meth = 0;
+my $rv_meth = 0;
 my $chr = "";
 
-open INFILE, "<:gzip" ,$input or die, print "Cannot open $input for reading\n";
+open INFILE, "<:gzip", $input || die "Cannot open $input for reading\n";
 open OUTFILE, ">$output" || die "Cannot open $output for writing.\n";
 print OUTFILE "chr\tstart\tend\tnum_C_fw\tnum_total_fw\tmeth_fw\tnum_C_rv\tnum_total_rv\tmeth_rv\ttotal_C\ttotal\ttotal_meth\n";
 
 while ($temp = <INFILE>) {
-    chomp($temp);
-    @temparray = split("\t",$temp); ## tab separated
+    chomp $temp;
+    @temparray = split("\t", $temp); ## tab separated
     $strand = $temparray[2];
     $chr = $temparray[0];
 
     if ($strand eq "+") {
-        $CpGid = $temparray[0].".".$temparray[1];
+        $CpGid = $temparray[0] . "." . $temparray[1];
         $CpGfwReadsHash{$CpGid} = ($temparray[3] + $temparray[4]);
         $CpGfwMethReadsHash{$CpGid} = $temparray[3];
 
@@ -70,14 +76,14 @@ while ($temp = <INFILE>) {
         }
     } else {
         $fwcoordinate = $temparray[1] - 1;
-        $CpGid = $temparray[0]. "." .$fwcoordinate;
+        $CpGid = $temparray[0] . "." . $fwcoordinate;
         $CpGrvReadsHash{$CpGid} = ($temparray[3] + $temparray[4]);
         if (exists $CpGfwReadsHash{$CpGid}) {
-            $CpGtotalReadsHash{$CpGid} = ($CpGfwReadsHash{$CpGid} + $CpGrvReadsHash{$CpGid} ) ;
-            $CpGtotalMethReadsHash{$CpGid} = ($CpGfwMethReadsHash{$CpGid} + $temparray[3] ) ;
+            $CpGtotalReadsHash{$CpGid} = ($CpGfwReadsHash{$CpGid} + $CpGrvReadsHash{$CpGid});
+            $CpGtotalMethReadsHash{$CpGid} = ($CpGfwMethReadsHash{$CpGid} + $temparray[3]);
         } else {
-            $CpGtotalReadsHash{$CpGid} = $CpGrvReadsHash{$CpGid} ;
-            $CpGtotalMethReadsHash{$CpGid} = $temparray[3] ;
+            $CpGtotalReadsHash{$CpGid} = $CpGrvReadsHash{$CpGid};
+            $CpGtotalMethReadsHash{$CpGid} = $temparray[3];
         }
 
         if ($temparray[3] == 0) {
@@ -89,12 +95,12 @@ while ($temp = <INFILE>) {
         if ($CpGtotalReadsHash{$CpGid} > 0) {
             $total_meth = sprintf("%.2f", (($CpGtotalMethReadsHash{$CpGid}/$CpGtotalReadsHash{$CpGid})*100));
             print OUTFILE "$chr\t$fwcoordinate\t$temparray[1]\t$CpGfwMethReadsHash{$CpGid}\t$CpGfwReadsHash{$CpGid}\t$fw_meth\t$temparray[3]\t$CpGrvReadsHash{$CpGid}\t$rv_meth\t$CpGtotalMethReadsHash{$CpGid}\t$CpGtotalReadsHash{$CpGid}\t$total_meth\n";
-            %CpGfwHash=();
-            %CpGrvHash=();
-            %CpGfwReadsHash=();
-            %CpGrvReadsHash=();
-            %CpGfwMethReadsHash=();
-            %CpGrvMethReadsHash=();
+            %CpGfwHash = ();
+            %CpGrvHash = ();
+            %CpGfwReadsHash = ();
+            %CpGrvReadsHash = ();
+            %CpGfwMethReadsHash = ();
+            %CpGrvMethReadsHash = ();
         }
     }
 }
@@ -103,7 +109,7 @@ print "Finish reading file $input\n";
 close(OUTFILE);
 
 sub print_helpfile{
-    print "\n", '='x100,"\n";
+    print "\n", '='x100, "\n";
     print ">>> USAGE: perl methylProfile.bismark.pl [Options] <<<\n\n";
     print "Options -i|--input: input file of bismark CpG report gz file.\n";
     print "Options -o|--outfile: output file of combined CpG profile from both forward and reverse strands.\n";
