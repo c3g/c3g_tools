2938dcf3962d3138fee38c0287e8c38e808f2c8e
Eloi Mercier <emercier@jonquille.genome.mcgill.ca>
2017-10-17 13:46:20 -0400

    adding scripts for spike-in step in illumina pipeline: plotSpikeInCount.R, filterBastOutput.py, estimateSpikeInCount.sh

---
+++ R-tools/plotSpikeInCount.R
@@ -0,0 +1,88 @@
+#!/usr/bin/env Rscript
+
+args = commandArgs(trailingOnly=TRUE)
+# args[1] filter blast file
+# args[2] total number of reads sampled
+
+file_name=args[1]
+data=read.delim(file_name, as.is=TRUE)
+#check that not empty
+tags=data[,"Tag"]
+total_tags=nrow(data)
+alignments=data[,"Alignment"]
+identity=data[,"Identity"]
+mismatches=data[,"Mismatches"]
+samples_size=as.integer(args[2])
+
+tags.table.count=table(tags)
+tags.table.count=tags.table.count[order(tags.table.count, decreasing=T)]
+tags.table=tags.table.count/samples_size*100
+tag_color=c("lightblue",rainbow(length(tags.table)-1, alpha = 1))
+alignments.table=table(tags,alignments)/total_tags*100
+alignments.table=alignments.table[names(tags.table), ,drop=F] #to avoid losing one dimension if only 1 column
+identity.table=table(tags,identity)/total_tags*100
+identity.table=identity.table[names(tags.table), , drop=F]
+mismatches.table=table(tags,mismatches)/total_tags*100
+mismatches.table=mismatches.table[names(tags.table), ,drop=F]
+
+# IC at 95% for binomial distribution
+IC95_bn<-function(p, n){
+	d=sqrt(p*(1-p)/n)*1.96
+	ic=c(p-d, p+d)
+	return(ic)}
+
+
+pdf(paste0(file_name,".pdf"))
+if(nrow(data)>0) {
+	tags.ic95.count=sapply(tags.table.count, function(x) {IC95_bn(x/samples_size, samples_size)})*samples_size
+	tags.ic95=sapply(tags.table, function(x) {IC95_bn(x/100, samples_size)*100})
+	m <- matrix(c(1,1,2,3,4,5),nrow = 3,ncol = 2, byrow = TRUE)
+	layout(mat = m,heights = c(0.1,0.45,0.45))
+	#legend
+	par(mar = c(0,0,2,0))
+	plot(1, type = "n", axes=FALSE, xlab="", ylab="", main=basename(file_name),cex.main=1)
+	legend(x = "top",inset = 0, legend = names(tags.table), fill=tag_color, border=tag_color, horiz = TRUE, title=paste0(samples_size, " sampled reads"))
+	par(mar=c(4, 4, 4, 2) + 0.1)
+	#tag count
+	if (length(tags.table)>5){
+		tags.bp<-barplot(tags.table.count,main="Tags", col=tag_color,border=tag_color, ylab="Read count", names.arg=tags.table.count, ylim=c(0, max(tags.table.count,tags.ic95.count[2,])), las=3)
+	} else {
+		tags.bp<-barplot(tags.table.count,main="Tags", col=tag_color,border=tag_color, ylab="Read count", names.arg=paste(tags.table.count, names(tags.table),sep="\n"), ylim=c(0, max(tags.table.count,tags.ic95.count[2,])))
+	}
+	suppressWarnings(arrows(tags.bp, tags.ic95.count[1,], tags.bp, tags.ic95.count[2,], angle=90, code=3, length=0.1))
+	#tag proportion
+	if (length(tags.table)>5){
+		tags.bp<-barplot(tags.table,main="Tags", col=tag_color,border=tag_color, ylab="% of reads", names.arg=paste0(signif(tags.table,3),"%"), ylim=c(0, max(tags.table,tags.ic95[2,])), las=2)
+	} else {
+		tags.bp<-barplot(tags.table,main="Tags", col=tag_color,border=tag_color, ylab="% of reads", names.arg=paste(paste0(signif(tags.table,3),"%"), names(tags.table),sep="\n"), ylim=c(0, max(tags.table,tags.ic95[2,])))
+	}
+	suppressWarnings(arrows(tags.bp, tags.ic95[1,], tags.bp, tags.ic95[2,], angle=90, code=3, length=0.1))
+	#aligment length
+	barplot(alignments.table, main="Alignment length", col=tag_color, border=tag_color, ylab="% of alignments")
+	# barplot(identity.table, main="Identity", col=tag_color, ylab="% of alignments", names.arg=signif(as.numeric(colnames(identity.table)),3))
+	#mismatches
+	barplot(mismatches.table, main="Mismatches", col=tag_color, border=tag_color, ylab="% of alignments")
+	par(mar=c(5, 4, 4, 2) + 0.1)
+} else {
+	m <- matrix(c(1,1,2,3,4,5),nrow = 3,ncol = 2, byrow = TRUE)
+	layout(mat = m,heights = c(0.1,0.45,0.45))
+	par(mar = c(0,0,2,0))
+	plot(1, type = "n", axes=FALSE, xlab="", ylab="", main=basename(file_name),cex.main=1)
+	text(1,1,labels=c("No tag detected"))
+	for (i in 1:4){
+		plot(1, type = "n", axes=FALSE, xlab="", ylab="", main="",cex.main=0.7)
+	}
+}
+out <- dev.off()
+
+tags.summary=data.frame(matrix(NA, ncol=4, nrow=length(tags.table)))
+colnames(tags.summary)=c("Tag", "Count", "Estimate_%" ,"IC95")
+if(nrow(data)>0) {
+	tags.summary[,"Tag"]=names(tags.table)
+	tags.summary[,"Count"]=tags.table.count
+	tags.summary[,"Estimate_%"]=tags.table
+	tags.summary[,"IC95"]=paste0("[",signif(tags.ic95[1,],3),",", signif(tags.ic95[2,],3),"]")
+}
+write.table(tags.summary, file=paste0(file_name,".summary"), quote=F, sep="\t", col.names=T, row.names=F)
+
+print("PDF and summary files successfully generated")
---
+++ tools/estimateSpikeInCount.sh
@@ -0,0 +1,78 @@
+#!/bin/sh
+#modified version of runBlast.sh
+#by Eloi Mercier
+
+set -e
+echo "STARTING: " $(date)
+
+#get args
+SAMPLE_TO=$1
+OUTPUT_DIR=$2
+FILE_1=$3
+DATABASE=$4
+ALIGN_SIZE=$5
+MISTMATCHES=$6
+IDENTITY=$7
+
+FILE_NAME=$(basename $FILE_1)
+
+#params
+if [[ $FILE_1 =~ \.gz$ ]]; then
+	FPR="fastqPickRandom.pl --compressed --threshold "
+	read_method=zcat
+else
+	FPR="fastqPickRandom.pl --threshold "
+	read_method=cat
+fi
+FQ2FA="fastq2FastaQual.pl"
+BLAST="blastn -query"
+
+#check output directory
+if [ ! -d $OUTPUT_DIR/ ]; then
+  mkdir $OUTPUT_DIR
+fi
+
+#check database
+if [ ! -e $DATABASE.nsd ]; then #.nsd, .nsi, .nsq, .nsh, .nin all created by makeblastdb; check for all?
+	echo "generating blast database"
+	makeblastdb -in $DATABASE -parse_seqids -dbtype nucl
+fi
+
+Nseq=$($read_method $FILE_1 | awk ' { if (substr($0,0,1) == "+") { print $0} }' | wc -l)
+echo "$FILE_1 has $Nseq sequences"
+#Get the threshold of random picking
+thrC=$(echo " scale=6; $SAMPLE_TO / $Nseq" | bc)
+echo "$FILE_1 has 0$thrC rdp threshold"
+
+###################
+#1. Random pick
+###################
+$FPR 0$thrC --input1 $FILE_1 --out1 $OUTPUT_DIR/$FILE_NAME.subSampled_${SAMPLE_TO}.fastq
+
+###################
+#2. Format fastq to fasta
+###################
+$FQ2FA $OUTPUT_DIR/$FILE_NAME.subSampled_${SAMPLE_TO}.fastq $OUTPUT_DIR/$FILE_NAME.subSampled_${SAMPLE_TO}.fasta $OUTPUT_DIR/$FILE_NAME.subSampled_${SAMPLE_TO}.qual
+
+###################
+#3. Blast fasta
+###################
+$BLAST $OUTPUT_DIR/$FILE_NAME.subSampled_${SAMPLE_TO}.fasta -db $DATABASE -out $OUTPUT_DIR/$FILE_NAME.subSampled_${SAMPLE_TO}.blast -max_target_seqs 1  -outfmt "6 qseqid qlen sseqid slen pident length mismatch qstart qend sstart send evalue bitscore" -ungapped
+
+###################
+#4. Filter blast output
+###################
+
+python /home/emercier/bitbucket/spikein_dev/mugqic_tools/python-tools/filterBastOutput.py --input_file $OUTPUT_DIR/$FILE_NAME.subSampled_${SAMPLE_TO}.blast --tag_file $DATABASE --output_dir $OUTPUT_DIR --align_length_min $ALIGN_SIZE --mismatches_max $MISTMATCHES --perc_identity_min $IDENTITY #log file?
+
+###################
+#5. Create report and graphs
+###################
+if [[ $IDENTITY != *.* ]]; then #treat 0 and 1 cases -> 0.0 and 1.1
+	IDENTITY=$IDENTITY.0
+fi
+IDENTITY_PERC=${IDENTITY//./} #0.0 -> 00
+########REMOVE HARD CODED PATH
+Rscript --vanilla /home/emercier/bitbucket/spikein_dev/mugqic_tools/R-tools/plotSpikeInCount.R $OUTPUT_DIR/$FILE_NAME.subSampled_${SAMPLE_TO}.blast.${ALIGN_SIZE}bp_${MISTMATCHES}MM_${IDENTITY_PERC}id $SAMPLE_TO
+
+
