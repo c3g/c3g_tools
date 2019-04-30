7654b6f7cbee484bba2c6b537cb78417728a6c05
P-O Quirion <pioliqui@gmail.com>
2019-04-30 15:33:02 -0400

    update createRmapFile.sh

--- tools/createRmapFile.sh
+++ tools/createRmapFile.sh
@@ -6,4 +6,4 @@ GENOME_DIGEST=$1
 RMAP_FILE=$2
 
 cut -f 1-3 $GENOME_DIGEST | \
-awk 'BEGIN {{FS=\t; OFS=\t"}} NR>2 {{if ($2 != $3) print $0, NR}}'  > $RMAP_FILE
+awk 'BEGIN {{FS="\t"; OFS="\t"}} NR>2 {{if ($2 != $3) print $0, NR}}'  > $RMAP_FILE
