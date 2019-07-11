6b62ccc3a1d39cdb503f4bc3207222ac0170cd71
P-O Quirion <pioliqui@gmail.com>
2019-07-11 11:03:14 -0400

    bugfix cleanOTUtable

--- tools/cleanOTUtable.sh
+++ tools/cleanOTUtable.sh
@@ -6,6 +6,6 @@ OTU=$1
 TMP="_temp"
 BKP=$(echo $OTU | sed -e 's/\.txt$/_BACKUP.txt/')
 
-grep -v "^[a-zA-Z]..*$" $OTU > $TMP && \\
-cp $OTU $BKP && \\
+grep -v "^[a-zA-Z]..*$" $OTU > $TMP && \
+cp $OTU $BKP && \
 mv $TMP $OTU
--- tools/createBaitMapFile.sh
+++ tools/createBaitMapFile.sh
@@ -1,4 +1,4 @@
-#!/bin/env sh
+#!/usr/bin/env sh
 # 
 ##2018-03-15 - Edouard Henrion - edouard.henrion@computationalgenomics.ca
 #Usage createBaitMapFile.sh INPUT_FILE($1) BAIT_FILE($2) SORTED_BAIT_FILE($3) ANNOTATION($4) TMP_FILE($5) OUTPUT_FILE($6)
