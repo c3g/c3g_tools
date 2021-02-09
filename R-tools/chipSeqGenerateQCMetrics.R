ab9b34eb0683ae0b1392b9cd24cf1930f7d750a4
Paul Stretenowich <paul.stretenowich@gmail.com>
2021-02-09 18:32:26 -0500

    Switching from design to readser

--- R-tools/chipSeqGenerateQCMetrics.R
+++ R-tools/chipSeqGenerateQCMetrics.R
@@ -1,19 +1,23 @@
 # process.R
 
 args <- commandArgs(TRUE)
-design.list<-args[1]
+readset.list<-args[1]
 output_dir<-args[2]
-designs<-read.table(design.list, header=T, sep="\t", check.names=F)
+readsets<-read.table(readset.list, header=T, sep="\t", check.names=F)
 
-for(i in 1:nrow(designs)) {
+# readset<-read.table("~/Mount_points/beluga/home/scratch/chipseq_design_change/IHEC_test/readset.IHEC_test.txt", header=T, sep="\t", check.names=F)
+# output_dir<-"~/Mount_points/beluga/home/scratch/chipseq_design_change/IHEC_test"
 
-sampleName=designs[i,1]
-postscript(paste(output_dir, "/graphs/", sampleName, "_QC_Metrics.ps",sep=""))
+for(i in 1:nrow(readsets)) {
+
+sample_name=readset$Sample[i]
+mark_name=readset$MarkName[i]
+postscript(paste(output_dir, "/graphs/", sample_name, ".", mark_name, "_QC_Metrics.ps", sep=""))
 par(mfrow=c(2,2))
 
 # Tag count distribution
 
-countDist<-paste(output_dir, "/tags/", sampleName, "/tagCountDistribution.txt", sep="")
+countDist<-paste(output_dir, "/tags", sample_name, mark_name, "tagCountDistribution.txt", sep="/")
 d2<-read.table(countDist, header=T, sep="\t")
 d2<-subset(d2, d2[,1]<=7)
 mean<-0
@@ -21,23 +25,23 @@ for(j in 1:nrow(d2)) {
 	mean<-mean+(d2[j,2]*d2[j,1])
 }
 
-barplot(d2[2:10,2], names.arg=d2[2:10,1],ylim=c(0,1),xlab="Number of Tags per Genomic Position", ylab="Fraction of Genomic Positions", main=paste(sampleName, "\nTag count: ", round(mean,3), " avg tag per position", sep=" "), col="blue")
+barplot(d2[2:10,2], names.arg=d2[2:10,1],ylim=c(0,1),xlab="Number of Tags per Genomic Position", ylab="Fraction of Genomic Positions", main=paste(sample_name, "\nTag count: ", round(mean,3), " avg tag per position", sep=" "), col="blue")
 
 # Tag auto correlation
 
-countDist<-paste(output_dir, "/tags/", sampleName, "/tagAutocorrelation.txt", sep="")
+countDist<-paste(output_dir, "/tags", sample_name, mark_name, "tagAutocorrelation.txt", sep="/")
 d1<-read.table(countDist, header=T, sep="\t")
 maxH<-max(max(d1[,2]),max(d1[,3]))
 
-plot(d1[,1], d1[,2] ,xlab="Distance from reference tag (bp)", ylab="Number of observations", xlim=c(-300,400), ylim=c(0,maxH+500),main=paste(sampleName, " \nTag autocorrelation", sep=" "), col="blue", type='l')
+plot(d1[,1], d1[,2], xlab="Distance from reference tag (bp)", ylab="Number of observations", xlim=c(-300,400), ylim=c(0,maxH+500), main=paste(sample_name, " \nTag autocorrelation", sep=" "), col="blue", type='l')
 lines(d1[,1], d1[,3], col="red")
 legend("topleft", c("same strand","opposite strand"), col=c("blue", "red"), pch='-', pt.cex=3,bty='n') 
 
 # Sequence bias
 
-countDist<-paste(output_dir, "/tags/", sampleName, "/tagFreq.txt", sep="")
+countDist<-paste(output_dir, "/tags", sample_name, mark_name, "tagFreq.txt", sep="/")
 d1<-read.table(countDist, header=T, sep="\t")
-plot(d1[,1], d1[,2], xlab="Distance from 5' end of tags", ylab="Nucleotide Frequency", col="blue", xlim=c(-50,100), , ylim=c(0,0.45),type='l',main=paste(sampleName, "\nSequence bias", sep=" "))
+plot(d1[,1], d1[,2], xlab="Distance from 5' end of tags", ylab="Nucleotide Frequency", col="blue", xlim=c(-50,100), ylim=c(0,0.45), type='l', main=paste(sample_name, "\nSequence bias", sep=" "))
 lines(d1[,1],d1[,3], col="red")
 lines(d1[,1],d1[,4], col="yellow")
 lines(d1[,1],d1[,5], col="green")
@@ -50,14 +54,14 @@ legend("bottomright", c("A","C","G","T"), col=c("blue","red","yellow","green"), 
 
 # GC bias
 
-countGenome<-paste(output_dir, "/tags/", sampleName, "/tagGCcontent.txt", sep="")
-countIP<-paste(output_dir, "/tags/", sampleName, "/genomeGCcontent.txt", sep="")
+countGenome<-paste(output_dir, "/tags", sample_name, mark_name, "tagGCcontent.txt", sep="/")
+countIP<-paste(output_dir, "/tags", sample_name, mark_name, "genomeGCcontent.txt", sep="/")
 d1<-read.table(countGenome, header=T, sep="\t")
 d2<-read.table(countIP, header=T, sep="\t")
 
 maxH<-max(max(d1[,3]),max(d2[,3]))
 
-plot(d1[,1], d1[,3], col="black", type='l', xlab="GC content of ChIP fragments", ylim=c(0,maxH),ylab="Normalized Fraction of total fragments", main=paste(sampleName, "\n GC bias", sep=" "))
+plot(d1[,1], d1[,3], col="black", type='l', xlab="GC content of ChIP fragments", ylim=c(0,maxH),ylab="Normalized Fraction of total fragments", main=paste(sample_name, "\n GC bias", sep=" "))
 lines(d2[,1],d2[,3], col="red")
 
 legend("topright", c("Genome", "ChIP sample"), col=c("black","red"), pch="-", bty='n', pt.cex=3)
