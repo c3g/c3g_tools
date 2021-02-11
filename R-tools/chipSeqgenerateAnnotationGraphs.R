6617b09dea60ca59f3aaa387b802291c262e23f5
Paul Stretenowich <paul.stretenowich@gmail.com>
2021-02-10 19:06:40 -0500

    Switching from design to readser

--- R-tools/chipSeqgenerateAnnotationGraphs.R
+++ R-tools/chipSeqgenerateAnnotationGraphs.R
@@ -86,16 +86,16 @@ for (sample_name in ls(samples_hash_table)) {
 # Generate table stats with number of peaks, etc
 
 for (sample_name in ls(samples_hash_table)) {
-    toPrint<-rbind(c("readset", "group" , "number of peaks", "percent near tss", "median peak height", "highest peak", "lowest peak", "avg peak width"))
+    toPrint<-rbind(c("Sample", "Mark Name", "Group" , "Number of peaks", "Percent near tss", "Median peak height", "Highest peak", "Lowest peak", "Avg peak width"))
     for (mark_name in ls(samples_hash_table[[sample_name]])) {
         mark_type <- samples_hash_table[[sample_name]][[mark_name]]
         if(mark_type == "N") {
             narrow.peaks=TRUE;
-            designName<-paste(sample_name, mark_name, sep=".")
+            # designName<-paste(sample_name, mark_name, sep=".")
             fileDir=file.path(output_dir,"peak_call")
             annotationDir=file.path(output_dir,"annotation")
             # listFile=file.path(fileDir, list.files(fileDir, pattern=paste(designName, ".tss.stats.csv", "$", sep=""), recursive=T))
-            listFile=file.path(fileDir,list.files(fileDir, pattern=paste(designName,"_peaks.(narrow|broad)Peak", "$", sep=""), recursive=T))
+            listFile=file.path(fileDir,list.files(fileDir, pattern=paste(mark_name,"_peaks.(narrow|broad)Peak", "$", sep=""), recursive=T))
             print(paste("NOTICE: Processing peaks files to generate stats: ", paste(listFile, collapse=","),sep=" "))      
             for (fi in listFile){
                 averagePeakWidth<-NA
@@ -124,7 +124,7 @@ for (sample_name in ls(samples_hash_table)) {
                     d2<-subset(d1, d1[,1]>-1000 & d1[,1]<1000)
                     percentNearTSS<-round(nrow(d2)/nrow(d1), 2)*100
                 }
-                toPrint<-rbind(toPrint, c(designName, designGroup, nPeaks, percentNearTSS, medianPeakHeight, highestPeak, lowestPeak, averagePeakWidth))
+                toPrint<-rbind(toPrint, c(sample_name, mark_name, designGroup, nPeaks, percentNearTSS, medianPeakHeight, highestPeak, lowestPeak, averagePeakWidth))
                 print(toPrint)
             }
         }
