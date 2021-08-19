cc84f72438b203755cf853d4f1082490140ad80f
P-O Quirion <pioliqui@gmail.com>
2021-08-18 20:29:04 -0400

    removing extra \

--- tools/cleanOTUtable.sh
+++ tools/cleanOTUtable.sh
@@ -6,5 +6,6 @@ OTU=$1
 TMP="_temp"
 BKP=$(echo $OTU | sed -e 's/\.txt$/_BACKUP.txt/')
 
-grep -v "^[a-zA-Z]..*$" $OTU > $TMP && ls $(dirname $TMP) > /dev/null && \\
-cp $OTU $BKP && mv $TMP $OTU
+grep -v "^[a-zA-Z]..*$" $OTU > $TMP \
+  && ls $(dirname $TMP) > /dev/null \
+  && cp $OTU $BKP && mv $TMP $OTU
