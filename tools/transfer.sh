3df3cf90b1d2deae5df578851d048725f7bc9c79
Hector Galvez <jose.hector.galvez@computaitonalgenomics.ca>
2021-08-30 17:57:42 -0400

    Fix to the rsync copy issue provoked by the slash at the end of the input arguemnt (fix by Paul S.)

--- tools/transfer.sh
+++ tools/transfer.sh
@@ -3,7 +3,7 @@ set -eu -o pipefail
 
 usage () {
 echo "$0 <n_thread> <source> <dest>"
-echo "a threaded copy tool" 
+echo "a threaded copy tool that will rsync <source> (without '/' at the end of the path) into <dest>"
 }
 
 if [[ $# -ne 3 ]] ; then
@@ -14,11 +14,16 @@ export SOURCE_DIR=$2
 export DEST_DIR=$3
 export threads=$1
 
+if [ ${DEST_DIR: -1} == "/" ];
+then
+    export DEST_DIR=${DEST_DIR:: -1}
+fi
+
 mkdir -p  $DEST_DIR 2>/dev/null
 # sync folder structure first
 rsync -a -f'+ */' -f'- *' $SOURCE_DIR $DEST_DIR
 
 # cwd
-cd $SOURCE_DIR
-
-find . -type f -print0 |  parallel -0 --linebuffer --jobs $threads 'rsync -av {} $DEST_DIR/{//}/'
+cd $SOURCE_DIR/..
+SOURCE_DIR_LAST=`basename $SOURCE_DIR`
+find $SOURCE_DIR_LAST/ -type f -print0 |  parallel -0 --linebuffer --jobs $threads 'rsync -av {} $DEST_DIR/{//}/'
