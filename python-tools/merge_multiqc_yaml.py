198824a53bdf1c3646fb90baf119b5405d96fd2b
Paul Stretenowich <paul.stretenowich@gmail.com>
2021-12-09 19:09:32 -0500

    python tools - Fixing indentation error

--- python-tools/merge_multiqc_yaml.py
+++ python-tools/merge_multiqc_yaml.py
@@ -52,7 +52,7 @@ def main():
     # dumping = yaml.YAML().indent(sequence=4, offset=2)
     if args.output:
         with open(args.output, 'w') as output_file:
-            yaml.dump(template, output_file, Dumper=yaml.RoundTripDumper).indent(sequence=4, offset=2)
+            yaml.dump(template, output_file, Dumper=yaml.RoundTripDumper, indent=2, block_seq_indent=4)
     else:
         yaml.dump(template, sys.stdout)
 
