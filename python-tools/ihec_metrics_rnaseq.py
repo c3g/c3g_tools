8278f1c54ebc0c10a4d6ac28dc0d7719b877f4c4
Edouard Henrion <edouard.henrion@mcgill.ca>
2022-10-28 15:51:25 -0400

    undo nundesired changes in ihec_metrics

--- python-tools/ihec_metrics_rnaseq.py
+++ python-tools/ihec_metrics_rnaseq.py
@@ -31,11 +31,10 @@ import glob
 
 ## extract genome assembly:
 genome = sys.argv[1]
-working_dir = sys.argv[2]
 
 ## open metrics/rnaseqRep/metrics.tsv file and load its content into an array
 
-metrics_file1 = os.path.join(working_dir, "metrics", "rnaseqRep", "metrics.tsv")
+metrics_file1 = "metrics/rnaseqRep/metrics.tsv"
 
 
 with open(metrics_file1) as f:
@@ -50,6 +49,7 @@ keep1 = ["Sample", "Duplication Rate of Mapped", "Intergenic Rate", "Intragenic 
 
 header = ["Sample", "IntragenicRate", "IntergenicRate",  "IntronicRate", "DuplicationRate", "StrandSpecificity"]
 
+
 columns_to_keep = []
 for i, name in enumerate(d[:1][0]):
     if name in keep1:
@@ -62,7 +62,7 @@ metrics1 = pd.DataFrame(d[1:,columns_to_keep], columns=header)
 header = ['Sample', 'RawReads', 'SurvivingReads', 'SurvivingReads_perc', 'AlignedReads',
         'AlignedReads_perc', 'AlternativeAlignments', 'AlternativeAlignments_perc', 'rRNAReads', 'rRNAReads_perc', 'Coverage', 'ExonicRate', 'GenesDetected']
 
-metrics_file2 = os.path.join(working_dir, "report", "trimAlignmentTable.tsv")
+metrics_file2 = "report/trimAlignmentTable.tsv"
 
 with open(metrics_file2) as f:
     reader = csv.reader(f, delimiter="\t")
@@ -70,6 +70,8 @@ with open(metrics_file2) as f:
 
 d = np.array(d)
 metrics2= pd.DataFrame(d[1:, :], columns=header)
+
+
 
 ## merge metrics1 and metrics 2 by Sample ID
 
@@ -82,13 +84,16 @@ samples = list(All['Sample'])
 MTreads={}
 
 for sample in samples:
-    cmd = "samtools view -c "+os.path.join(working_dir, "alignment", sample, sample+".sorted.mdup.bam")+" MT"
+    cmd = "samtools view -c alignment/{sample}/{sample}.sorted.mdup.bam MT".format(sample =sample)
     MTreads[sample] = int(os.popen(cmd).read().strip())
     if MTreads[sample] == 0:
-        cmd = "samtools view -c "+os.path.join(working_dir, "alignment", sample, sample+".sorted.mdup.bam")+" chrM"
+        cmd = "samtools view -c alignment/{sample}/{sample}.sorted.mdup.bam chrM".format(sample =sample)
         MTreads[sample] = int(os.popen(cmd).read().strip())
 
-MTreads = pd.DataFrame.from_dict(MTreads, orient='index')
+
+
+MTreads = pd.DataFrame.from_dict(MTreads, 
+                            orient='index')
 
 MTreads.index.name = 'Sample'
 MTreads.reset_index(inplace=True)
@@ -105,7 +110,7 @@ for sample in samples:
     TotalMapped = 0
     rRNA_mapped = 0
     ## get all the rRNA readsets per sample: metrics/Sample/Readset/Readset.rRNA.stats.tsv
-    readsetFile = f"{working_dir}/metrics/{sample}/*/*rRNA.stats.tsv"
+    readsetFile = "metrics/{sample}/*/*rRNA.stats.tsv".format(sample = sample)
     for myfile in glob.glob(readsetFile):
         with open(myfile) as f:
             reader = csv.reader(f, delimiter="\t")
@@ -114,7 +119,9 @@ for sample in samples:
             rRNA_mapped = rRNA_mapped + int(d[1,1])
     rRNAreads[sample] = (rRNA_mapped * 100.0)/TotalMapped
 
-rRNAreads = pd.DataFrame.from_dict(rRNAreads, orient='index')
+
+rRNAreads = pd.DataFrame.from_dict(rRNAreads, 
+                            orient='index')
 
 rRNAreads.index.name = 'Sample'
 rRNAreads.reset_index(inplace=True)
@@ -122,6 +129,8 @@ rRNAreads.reset_index(inplace=True)
 rRNAreads.columns = ['Sample','rRNA_perc']
 
 All = pd.merge(All, rRNAreads, on='Sample', how='outer')
+
+
 
 ## reorder columns:
 
@@ -132,9 +141,16 @@ All = All.loc[:,header]
 file_name = 'report/IHEC_metrics_rnaseq_All.txt'
 All.to_csv(file_name, sep='\t', index=False)
 
+
 ### write one file per sample:
 
 for sample in samples:   
     file_name = 'report/IHEC_metrics_rnaseq_' + sample +'.txt'
     sub = All.loc[All['Sample'] == sample] 
     sub.to_csv(file_name, sep='\t', index=False)
+
+
+
+
+
+
