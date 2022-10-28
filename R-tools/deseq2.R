69da92fc9f528d2c547f4d1f9b41e397d9178e4b
Edouard Henrion <edouard.henrion@mcgill.ca>
2022-10-28 15:47:31 -0400

    R-Scripts - Added batch efet corection to deseq2

--- R-tools/deseq2.R
+++ R-tools/deseq2.R
@@ -11,6 +11,7 @@ usage = function(errM) {
     cat("\nUsage : Rscript deseq2.R [option] <Value>\n")
     cat("       -d      : design file\n")
     cat("       -c      : raw count file\n")
+    cat("       -b      : batch file\n")
     cat("       -o      : output directory\n")
     cat("       -h      : this help\n\n")
 
@@ -18,14 +19,14 @@ usage = function(errM) {
 }
 
 set.seed(123456789)
-perform_dge = function(counts, groups, count_limit, path) {
+perform_dge = function(counts, groups, batch, count_limit, path) {
 
     # Retain row which have > count_limit
     counts <- round(counts[rowSums(counts) > count_limit, ])
 
     # Normalize and do test
     coldata = data.frame(row.names=colnames(counts), condition=groups)
-    ddsFullCountTable = DESeq2::DESeqDataSetFromMatrix(countData = counts, colData=coldata, design=~condition)
+    ddsFullCountTable = DESeq2::DESeqDataSetFromMatrix(countData = counts, colData=coldata, design=~condition+batch)
 
     dds <- DESeq2::DESeq(ddsFullCountTable)
     res <- DESeq2::results(dds)
@@ -50,6 +51,7 @@ ARG = commandArgs(trailingOnly = T)
 ## default arg values
 count_limit = 9
 design_file = ""
+batch_file = ""
 rawcount_file = ""
 out_path = ""
 locfit = 0
@@ -58,6 +60,8 @@ locfit = 0
 for (i in 1:length(ARG)) {
     if (ARG[i] == "-d") {
         design_file = ARG[i+1]
+    } else if (ARG[i] == "-b") {
+        batch_file = ARG[i+1]
     } else if (ARG[i] == "-c") {
         rawcount_file = ARG[i+1]
     } else if (ARG[i] == "-o") {
@@ -79,6 +83,11 @@ if (!(file.exists(rawcount_file))) {
 if (out_path == "") {
     usage("Error : Output directory not specified")
 }
+if (batch_file != "") {
+    if (!(file.exists(batch_file))) {
+        usage("Error : Batch file not found")
+    }
+}
 
 # remove trailing "/" if necessary
 tmpOP = strsplit(out_path, "")
@@ -94,6 +103,14 @@ print(design)
 name_sample = as.character(as.vector(design[, 1]))
 countMatrix = round(rawcount[, 3:ncol(rawcount)])
 
+# Check if a batch file has to be used
+batches = ""
+if (batch_file != "") {
+    batches = read.csv2(batch_file, header=T, sep="\t", na.strings="0", check.names=F, colCalsses=c('character', 'character'))
+    # make sure design and batch are following the same sample order
+    merge_sorted <- merge(design, batches, sort=F, all.x=T, by.x=1)
+    batches <- merge_sorted[, 1:ncol(merge_sorted)]
+
 # Iterate over each design
 for (i in 2:ncol(design)) {
 
@@ -107,20 +124,22 @@ for (i in 2:ncol(design)) {
     current_design = design[, i]
     subsampleN = name_sample[!(is.na(current_design))]
     group = as.character(current_design)[!(is.na(current_design))]
-    groupN = unique(group)
     current_countMatrix = NULL
     for (j in 1:length(subsampleN)) {
         current_countMatrix = cbind(current_countMatrix, countMatrix[, is.element(colnames(countMatrix), subsampleN[j])])
     }
     colnames(current_countMatrix) = subsampleN
     rownames(current_countMatrix) = rawcount[, 1]
-    libSize <- colSums(current_countMatrix)
-    #geneSymbol = rawcount[, 2]
+
+    batch = ""
+    if (!(batches == "")) {
+        batch = as.character(batches)[!(is.na(current_design))]
+    }
 
     cat("Processing for the design\n")
     cat(paste("Name folder: ", name_folder, "\n", sep=""))
     cat(paste("Design : ", paste(subsampleN, group, sep="=", collapse=" ; "), "\n", spe=""))
 
     # Perform gene differential expressoin
-    perform_dge(current_countMatrix, group, count_limit, name_folder)
+    perform_dge(current_countMatrix, group, batch, count_limit, name_folder)
 }
