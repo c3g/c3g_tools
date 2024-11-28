01e4a40ee4386e45c15c63baeec84bb3ece40d9a
Mareike Janiak <mareike.janiak@computationalgenomics.ca>
2024-11-28 15:12:54 -0500

    limit parallel jobs for runBlast.sh

--- tools/runBlast.sh
+++ tools/runBlast.sh
@@ -62,7 +62,7 @@ else
 
   #Blast fasta
   #$BLAST $OUTPUT_PREFIX.R1R2.subSampled_${SAMPLE_TO}.fasta -db nt -out $OUTPUT_PREFIX.R1R2.subSampled_${SAMPLE_TO}.blastres -perc_identity 80 -num_descriptions 1 -num_alignments 1
-  $BLAST -file $OUTPUT_PREFIX.R1R2.subSampled_${SAMPLE_TO}.fasta --OUT $OUTPUT_PREFIX.R1R2.subSampled_${SAMPLE_TO}.blastres --BLAST 'blastn -db nt -perc_identity 80 -num_descriptions 1 -num_alignments 1'
+  $BLAST -file $OUTPUT_PREFIX.R1R2.subSampled_${SAMPLE_TO}.fasta --OUT $OUTPUT_PREFIX.R1R2.subSampled_${SAMPLE_TO}.blastres -n 4 --BLAST 'blastn -db nt -perc_identity 80 -num_descriptions 1 -num_alignments 1'
 
   #subselect only the species and report only the 20 most frequent
   grep ">" $OUTPUT_PREFIX.R1R2.subSampled_${SAMPLE_TO}.blastres | awk ' { print $2 "_" $3} ' | sort | uniq -c | sort -n -r | head -20 > $OUTPUT_PREFIX.R1R2.RDP.blastHit_20MF_species.txt
