adb8d51b21dd9f7cdb5c43e48d29b61d2fdb3c0c
Mareike Janiak <mareike.janiak@computationalgenomics.ca>
2025-09-04 12:56:56 -0400

    debug loading package with paste

--- R-tools/runQDNAseq.R
+++ R-tools/runQDNAseq.R
@@ -59,7 +59,7 @@ if (!dir.exists(outdir)) {
     dir.create(outdir, recursive = TRUE)
 }
 
-library(paste("QDNAseq.", reference, sep = ""))
+library(paste("QDNAseq.", reference, sep = ""), character.only = TRUE)
 
 # STEP 1: Load bins with specified size
 if (reference == "hg19") {
